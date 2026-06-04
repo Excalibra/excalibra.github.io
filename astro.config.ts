82953adc329813ef9bd04977d0d6e8f75786c7d6
𝕰𝖝𝖈𝖆𝖑𝖎𝖇𝖗𝖆 <83846602+Excalibra@users.noreply.github.com>
2026-06-04 23:49:55 +0000

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -14,6 +14,7 @@ import { SITE } from "./src/config";
 import react from "@astrojs/react";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
