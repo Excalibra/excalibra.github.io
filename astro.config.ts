a18f4d854e78d03638c77077b6d249a578882512
𝕰𝖝𝖈𝖆𝖑𝖎𝖇𝖗𝖆 <83846602+Excalibra@users.noreply.github.com>
2026-08-30 01:31:47 +0300

    Update Tailwind integration in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, envField } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
@@ -16,28 +16,33 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [sitemap({
-    filter: page => SITE.showArchives || !page.endsWith("/archives"),
-  }), react(), mdx()],
+  integrations: [
+    sitemap({
+      filter: (page) => SITE.showArchives || !page.endsWith("/archives"),
+    }),
+    react(),
+    mdx(),
+    tailwind(),
+  ],
   markdown: {
     remarkPlugins: [
       remarkMath,
-      remarkToc, [remarkCollapse, { test: "Table of contents" }]
+      remarkToc,
+      [remarkCollapse, { test: "Table of contents" }],
     ],
     rehypePlugins: [
       rehypeKatex,
-      [rehypeExternalLinks,
+      [
+        rehypeExternalLinks,
         {
-          target: "\_blank",
+          target: "_blank",
           rel: ["noopener", "noreferrer", "external"],
         },
       ],
     ],
     shikiConfig: {
-      // For more themes, visit https://shiki.style/themes
       themes: { light: "min-light", dark: "night-owl" },
       defaultColor: false,
       wrap: false,
@@ -47,16 +52,6 @@ export default defineConfig({
         transformerNotationWordHighlight(),
         transformerNotationDiff({ matchAlgorithm: "v3" }),
       ],
-    },
-  },
-  vite: {
-    // eslint-disable-next-line
-    // @ts-ignore
-    // This will be fixed in Astro 6 with Vite 7 support
-    // See: https://github.com/withastro/astro/issues/14030
-    plugins: [tailwindcss()],
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
     },
   },
   image: {
