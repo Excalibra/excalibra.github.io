f61ed830dc98e0b41168a828de21527764851175
𝕰𝖝𝖈𝖆𝖑𝖎𝖇𝖗𝖆 <83846602+Excalibra@users.noreply.github.com>
2026-08-30 15:27:48 +0300

    Create chart.js

---
+++ assets/js/chart.js
@@ -0,0 +1,20 @@
+function css(name) {
+  return "rgb(" + getComputedStyle(document.documentElement).getPropertyValue(name) + ")";
+}
+
+Chart.defaults.font.size = 14;
+Chart.defaults.plugins.colors.enabled = false;
+Chart.defaults.backgroundColor = css("--color-primary-300");
+Chart.defaults.elements.point.borderColor = css("--color-primary-400");
+Chart.defaults.elements.bar.borderColor = css("--color-primary-500");
+Chart.defaults.elements.bar.borderWidth = 1;
+Chart.defaults.elements.line.borderColor = css("--color-primary-400");
+Chart.defaults.elements.arc.backgroundColor = css("--color-primary-200");
+Chart.defaults.elements.arc.borderColor = css("--color-primary-500");
+Chart.defaults.elements.arc.borderWidth = 1;
+
+const prefersReducedMotion = window.matchMedia?.("(prefers-reduced-motion: reduce)").matches;
+Chart.defaults.animation.duration = prefersReducedMotion ? 0 : 250;
+Chart.defaults.animation.easing = 'easeOutQuart';
+Chart.defaults.datasets.doughnut.animation.animateRotate = !prefersReducedMotion;
+Chart.defaults.datasets.doughnut.animation.animateScale = false;
